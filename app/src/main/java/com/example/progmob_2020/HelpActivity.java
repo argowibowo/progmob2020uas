342c613538086a97d2f5d370886823f661f4ec90
jonathan_mata <jonathan.leonardo@si.ukdw.ac.id>
2020-10-01 21:02:34 +0800

    Video Tutorial 1

---
+++ app/src/main/java/com/example/progmob_2020/HelpActivity.java
@@ -0,0 +1,21 @@
+package com.example.progmob_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        TextView txtHelp = (TextView)findViewById(R.id.textViewHelp);
+
+        Bundle b = getIntent().getExtras();
+        String textHelp = b.getString("help_string");
+        txtHelp.setText(textHelp);
+    }
+}
--- app/src/main/java/com/example/progmob_2020/MainActivity.java
+++ app/src/main/java/com/example/progmob_2020/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.progmob_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable
+        final TextView txtView = (TextView) findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button) findViewById((R.id.button1));
+        final EditText myEditText = (EditText)findViewById(R.id.editText1);
+        final Button btnHelp = (Button)findViewById(R.id.btnHelp);
+
+        //action
+        txtView.setText(R.string.text_hello_world);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("COBA KLIK", myEditText.getText().toString());
+                txtView.setText(myEditText.getText().toString());
+            }
+        });
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HelpActivity.class);
+                Bundle b = new Bundle();
+
+                b.putString("help_string", myEditText.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
     }
 }
