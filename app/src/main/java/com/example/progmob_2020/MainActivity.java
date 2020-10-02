b35c6cb603fc8ff2a90fd4ede09e4f14a0011797
jonathan_mata <jonathan.leonardo@si.ukdw.ac.id>
2020-10-02 13:44:25 +0800

    Video Tutorial 2

--- app/src/main/java/com/example/progmob_2020/MainActivity.java
+++ app/src/main/java/com/example/progmob_2020/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tes_constraint_layout);
 
         //variable
         final TextView txtView = (TextView) findViewById(R.id.mainActivityTextView);
