d064cf1782ee1d8d6e7148fcb8802c2e5ac6f27d
jonathan_mata <jonathan.leonardo@si.ukdw.ac.id>
2020-10-09 17:26:22 +0800

    (3) Recycler View

---
+++ app/src/main/java/com/example/progmob_2020/Pertemuan2/RecyclerActivity.java
@@ -0,0 +1,16 @@
+package com.example.progmob_2020.Pertemuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.progmob_2020.R;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}
---
+++ app/src/main/java/ukdw/com/progmob_2020/Pertemuan2/ListActivity.java
@@ -0,0 +1,57 @@
+package ukdw.com.progmob_2020.Pertemuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import ukdw.com.progmob_2020.R;
+
+
+public class ListActivity extends AppCompatActivity {
+
+    String[] items={"lorem", "ipsum", "dolor", "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi",
+            "vel","ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel","erat", "placerat", "ante","porttitor",
+            "sodales", "pellentesque", "augue", "purus"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ListView lv = (ListView)findViewById(R.id.lvLatihan);
+        Spinner sp = (Spinner)findViewById(R.id.spinnerLatihan);
+
+        lv.setAdapter(new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_list_item_1,items));
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(ListActivity.this,android.R.layout.simple_spinner_item,items);
+        sp.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListActivity.this,"Anda Memilih: " +  items[i],Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ListActivity.this,"Anda Memilih: " +  items[i],Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(ListActivity.this,"Anda Tidak Memilih",Toast.LENGTH_LONG);
+            }
+        });
+
+    }
+}
