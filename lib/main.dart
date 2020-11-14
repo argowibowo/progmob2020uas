43ceccf3f97fab686fc10f44dc8498bb9c894888
magdalenaevelyn <mevelynh@gmail.com>
2020-11-14 18:40:03 +0700

    Tugas 8: Flutter Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,91 @@
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter'),
+//         ),
+//         body: Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:progmob_flutter/pertemuan1.dart';
+import 'package:progmob_flutter/tugaspertemuan8.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // Tugas Hello World
+      // home: MyHomePage(title: "Flutter Demo Home Page"),
+      // Video Pertemuan 8
+      // home: Pertemuan1(title: "Home Page Buatan Sendiri"),
+      // Tugas Pertemuan 8
+      home: TugasPertemuan8(title: "Tugas Pertemuan 1"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Hello Magdalena Evelyn",
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            )
+          ],
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/tugaspertemuan8.dart
@@ -0,0 +1,101 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class TugasPertemuan8 extends StatefulWidget {
+  TugasPertemuan8({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _TugasPertemuan8State createState() => _TugasPertemuan8State();
+}
+
+class _TugasPertemuan8State extends State<TugasPertemuan8> {
+  // final _formKey = GlobalKey<FormState>();
+  final _text = TextEditingController();
+  bool _validate = false;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _text.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Center(
+        // key: _formKey,
+        child: Column(
+          children: <Widget>[
+            new Row(
+              children: <Widget>[
+                // new Container(
+                //   padding: EdgeInsets.all(10.0),
+                //   // child: new Image.asset('images/blue-person.png', width: 40.0, height: 40.0),
+                //   width: 20,
+                //   height: 20,
+                //   child: new Icon(Icons.person),
+                // ),
+                new Expanded(
+                    child: new Container(
+                      padding: EdgeInsets.all(14.0),
+                      child: new TextFormField(
+                        controller: _text,
+                        decoration: new InputDecoration(
+                            icon: Icon(Icons.people),
+                            labelText: "Nama Lengkap",
+                            hintText: "contoh: Argo Uchiha",
+                            border: OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(5),
+                            ),
+                            // labelText: 'Enter the Value',
+                            errorText: _validate ? 'Nama tidak boleh kosong' : null,
+                        ),
+                        // validator: (value){
+                        //   if(value.isEmpty){
+                        //     return "Nama tidak boleh kosong";
+                        //   }
+                        //   return null;
+                        // },
+                      ),
+                    ),
+                ),
+              ],
+            ),
+            new RaisedButton(
+              // onPressed: (){
+              //     if(_formKey.currentState.validate()){
+              //
+              //     }
+              //   },
+              onPressed: () {
+                setState(() {
+                  _text.text.isEmpty ? _validate = true : _validate = false;
+                });
+              },
+                color: Colors.blue,
+                // disabledColor: Colors.blue,
+              child: Text(
+                "Submit",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
