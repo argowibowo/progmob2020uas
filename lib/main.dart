94508f36a7fed44af22760bc60666723080f139d
felixrivaldo63 <felixr6300@gmail.com>
2020-11-14 16:38:49 +0700

    Pertemuan 8 & Tugas Pertemuan 8

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,91 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:progmob_setelah_uts_72180190/pertemuan1.dart';
+import 'package:progmob_setelah_uts_72180190/tugaspertemuan8.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter'),
+//         ),
+//         body: Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: Tugaspertemuan8(title: 'Tugas Pertemuan 1'),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
---
+++ lib/tugaspertemuan8.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Tugaspertemuan8 extends StatefulWidget {
+  Tugaspertemuan8({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _Tugaspertemuan8State createState() => _Tugaspertemuan8State();
+}
+
+class _Tugaspertemuan8State extends State<Tugaspertemuan8> {
+  String validateName(String value) {
+    if (value.isEmpty) {
+      return 'Nama Tidak Boleh Kosong !!';
+    }
+    return null;
+  }
+
+  final formKey = GlobalKey<FormState>();
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.all(10.0)),
+              TextFormField(
+                decoration: new InputDecoration(
+                    icon: Icon(Icons.people),
+                    labelText: "Nama Lengkap",
+                    hintText: "Contoh: Felix Rivaldo",
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(5),
+                    )),
+                validator: validateName,
+                onSaved: (String value) {
+                  name = value;
+                },
+              ),
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  "Simpan",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (formKey.currentState.validate())
+                    formKey.currentState.save();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
