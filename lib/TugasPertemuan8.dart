267cae01689542e0d2b7c1043463eb5ee5d541be
X541U\Personal <aldo.anggi@si.ukdw.ac.id>
2020-11-19 00:15:10 +0700

    Tugas Pertemuan 8

---
+++ lib/TugasPertemuan8.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TugasPertemuan8 extends StatefulWidget {
+  TugasPertemuan8({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TugasPertemuan8State createState() => _TugasPertemuan8State();
+}
+
+class _TugasPertemuan8State extends State<TugasPertemuan8>{
+  GlobalKey<FormState> key = GlobalKey<FormState>();
+
+  void validate(){
+    if(key.currentState.validate()){
+      print("Validated");
+    }else{
+      print("Failed");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tugas Pertemuan 8"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Center(
+          child: Form(
+            key: key,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "Nama Tidak Diperbolehkan Kosong";
+                    }else{
+                      return null;
+                    }
+                  },
+                  decoration: new InputDecoration(
+                    icon: Icon(
+                      Icons.people,
+                      color: Colors.lightBlue,
+                    ),
+                    labelText: "Nama Lengkap",
+                    hintText: "Contoh: Han Ji Pyeong",
+                    border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(3),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                    color: Colors.yellowAccent,
+                    onPressed: validate,
+                  child: Text(
+                    "Save",
+                    style: TextStyle(
+                      color: Colors.black
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
