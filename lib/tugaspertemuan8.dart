07bff4899513a6e90543752e44ae96b9183bfb90
riswantritama <riswanst@gmail.com>
2020-11-19 02:56:22 +0700

    Tugas 8:Flutter Widget

---
+++ lib/tugaspertemuan8.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class tugaspertemuan8 extends StatefulWidget {
+  tugaspertemuan8({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _tugaspertemuan8State createState() => _tugaspertemuan8State();
+}
+
+class _tugaspertemuan8State extends State<tugaspertemuan8> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validate(){
+    if(formkey.currentState.validate()){
+      print("validated");
+    }else{
+      print("failed");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tugas Pertemuan 8'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child:Center(
+            child:Form(
+              key:formkey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Nama Tidak Boleh Kosong";
+                      }else{
+                        return null;
+                      }
+                    },
+                    decoration: new InputDecoration(
+                      icon:Icon(
+                        Icons.people,
+                        color:  Colors.blue,
+                      ),
+                      labelText: "Nama Lengkap",
+                      hintText: "Contoh: Sulia Naara",
+                      border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(3),
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    onPressed:validate,
+                    child: Text(
+                      "Simpan",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
