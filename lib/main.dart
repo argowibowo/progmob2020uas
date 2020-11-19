9940ca2b272f7e8bbb13604779b6c5cb359792a2
LAPTOP-1LR8SJID\lenovo <fransisca.angelina@students.ukdw.ac.id>
2020-11-19 19:12:04 +0700

    Tugas 8: Flutter Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,81 @@
-import 'package:flutter/material.dart'; //bagian import
+import 'package:flutter/material.dart';
+import 'package:flutter_72180185/tugaspertemuan8.dart';
+//import 'pertemuan1.dart';
 
-void main() => runApp(MyApp()); //bagian main
-
-const String _title = 'Hello World';
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, //menghilangkan banner debug pada aplikasi
-        title: _title, //judul untuk recent app
-        home: Scaffold(
-          backgroundColor: Color(0x34495eaa), //HEX Colors
-          appBar: AppBar(
-              leading: Icon(Icons.home),
-              title: Text(_title), //judul untuk ditampilkan pada AppBar
-              backgroundColor: Color(0x666666aa) //HEX Colors
-          ),
-          body: Center(
-              child: Text('Hello World!', style: TextStyle(fontSize: 28, color: Colors.white))
-          ),
-        )
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TugasPertemuan8 (title: 'Tugas Pertemuan 8'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/tugaspertemuan8.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class TugasPertemuan8 extends StatefulWidget {
+  TugasPertemuan8({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _TugasPertemuan8State createState() => _TugasPertemuan8State();
+}
+class _TugasPertemuan8State extends State<TugasPertemuan8> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              // TextField(),
+              TextFormField(
+                decoration: new InputDecoration(
+                  hintText: "contoh: Fransisca Fethy Angelina",
+                  labelText: "Nama Lengkap",
+                  icon: Icon(Icons.people),
+                  border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(5.0)),
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              RaisedButton(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {}
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
