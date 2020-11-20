a872994d3a2e4a93043f925ec40dd21862f37791
Morgannnn <morgan.stephen@si.ukdw.ac.id>
2020-11-20 23:08:03 +0700

    Tugas 8 Tugas 8: Flutter Widget

--- lib/tugaspertemuan8.dart
+++ lib/tugaspertemuan8.dart
@@ -2,67 +2,67 @@ import 'package:flutter/material.dart';
 
 
 class tugaspertemuan8 extends StatefulWidget {
+  final String title;
+
   tugaspertemuan8({Key key, this.title}) : super(key: key);
 
-final String title;
 
 @override
 _tugaspertemuan8State createState() => _tugaspertemuan8State();
 }
 
 class _tugaspertemuan8State extends State<tugaspertemuan8> {
-  int _counter = 2;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+        "Pertemuan Pertama",
+        ),
 
-        title: Text(widget.title),
+
       ),
-      body: Center(
-        child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.start,
-
-          children: <Widget>[
-            new Icon(Icons.android, color: Colors.red[900], size: 40.0),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
             TextFormField(
-
-
+            style: TextStyle(
+              color: Colors.black,
+    ),
               decoration: new InputDecoration(
-
                   labelText: "Nama Lengkap",
                   hintText: "contoh: Morgan Haloho",
+                  icon: Icon(Icons.android),
                   border: OutlineInputBorder(
-                    borderRadius: new BorderRadius.only(topRight:Radius.circular(12)),
+                    borderRadius: new BorderRadius.only(topRight:Radius.circular(8)),
                   )
               ),
             ),
             RaisedButton(
-              color: Colors.transparent,
-              splashColor: Colors.blue,
-              onPressed: null,
-              child: Text("Simpan",
-                style: TextStyle(
-                    color: Colors.white
-                ),
-              ),
+            child: Container(
+              child: Text(
+              "Submit",
+              style: TextStyle(
+            fontSize: 18,
+            color: Colors.black,
+    ),
+    ),
+    ),
+              color: Colors.blue,
+
+
+
+
             )
           ],
+          ),
         ),
       ),
-      //floatingActionButton: FloatingActionButton(
-      //onPressed: _incrementCounter,
-      //tooltip: 'Increment',
-      //child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
