e89b5b1aec24c071dd2196f796b14a1565880fbd
JessyFriska <jessy.friska@si.ukdw.ac.id>
2020-11-20 18:49:56 +0700

    Flutter Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_progmob_2020/TugasPertemuan8.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TugasPertemuan8(title: 'Flutter Demo Home Page'),
     );
   }
 }
