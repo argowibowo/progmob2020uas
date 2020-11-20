16cb54743ea50e54adccc69b7ca27bf2e29ed41b
Ronaldo Merani <ronaldo.engelbert@si.ukdw.ac.id>
2020-11-20 20:51:38 +0700

    Video pertemuan 8 + Tugas8

---
+++ android/app/src/main/java/com/example/ronaldo_flutter_progmob_2020/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ronaldo_flutter_progmob_2020;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pertemuan1.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Pertemuan1 extends StatefulWidget {
+  Pertemuan1({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _Pertemuan1State createState() => _Pertemuan1State();
+}
+
+class _Pertemuan1State extends State<Pertemuan1> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: new InputDecoration(
+                labelText: "Input Text",
+                hintText: "Teks yang akan  di input formatnya adalah sbb",
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(5.0)),
+            TextFormField(
+              decoration: new InputDecoration(
+                labelText: "Input Text 2",
+                hintText: "Teks yang akan  di input formatnya adalah sbb",
+                border: OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(5),
+                )
+              ),
+            ),
+            RaisedButton(
+              color: Colors.black54
+              ,
+              child: Text(
+                "Simpan",
+                style: TextStyle(
+                  color: Colors.blue
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      //floatingActionButton: FloatingActionButton(
+      //onPressed: _incrementCounter,
+      // tooltip: 'Increment',
+      //child: Icon(Icons.add),
+      //), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
