68b00a556165ed7ebb4ee2ebe82f00ad6ce20cac
tatans <anantha.ginting@si.ukdw.ac.id>
2020-11-20 16:22:01 +0700

    pertemuan 8

---
+++ android/app/src/main/java/com/example/flutter_72170134/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_72170134;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pertemuan8.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+class Pertemuan8 extends StatefulWidget {
+  Pertemuan8({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _Pertemuan8State createState() => _Pertemuan8State();
+}
+
+class _Pertemuan8State extends State<Pertemuan8> {
+  int _counter = 2;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: new InputDecoration(
+                labelText: "Nama Lengkap",
+                hintText: "Anantha Ginting",
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.all(5.0)
+            ),
+            TextFormField(
+              decoration: new InputDecoration(
+                  labelText: "NIM",
+                  hintText: "Contoh 72170134",
+                  border: OutlineInputBorder(
+                      borderRadius: new BorderRadius.circular(5)
+                  )
+              ),
+            ),
+            RaisedButton(
+              focusColor: Colors.blue,
+              color: Colors.blue,
+              child: Text(
+                "Simpan",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            )
+
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
