f77c6581aaa0e5ca9899b57071d87b17facab81f
effandito <mikhael.effandito@si.ukdw.ac.id>
2020-11-20 23:10:07 +0700

    Tugas dan Pertemuan 8

---
+++ lib/Tugas8.dart
@@ -0,0 +1,71 @@
+import  'package:flutter/material.dart';
+
+class Tugas8 extends StatefulWidget {
+  Tugas8({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _Tugas8State createState() => _Tugas8State();
+}
+
+class _Tugas8State extends State<Tugas8>{
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  void validate(){
+    if(formkey.currentState.validate()){
+      print("validated");
+    }else{
+      print("failed");
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tugas Pertemuan 8'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+          child: Form(
+            key: formkey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "wajib di isi";
+                      }
+                      else{
+                        return null;
+                      }
+                    },
+                    decoration: new InputDecoration(
+                        labelText: "Nama Lengkap",
+                        hintText: "Nama Lengkap",
+                        icon: Icon(Icons.account_circle),
+                        border: OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(5)
+                        )
+                    ),
+                  ),
+                  RaisedButton(
+                    //: Colors.blue,
+                    color: Colors.blue,
+                    onPressed: validate,
+                    child: Text(
+                      "Simpan",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                  )
+                ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
