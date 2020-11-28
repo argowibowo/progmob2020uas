42c242b3867978f5dee7918c19dac4bc831d88e9
kerenkezia12 <keren.kezia@si.ukdw.ac.id>
2020-11-28 23:16:21 +0700

    Tugas 9 selesai

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_keren/login.dart';
-import 'package:flutter_keren/pertemuan1.dart';
-import 'package:flutter_keren/Splashscreen.dart';
-import 'package:flutter_keren/tugaspertemuan8.dart';
 
 void main() {
-  runApp(MyApp()
-  );
+  runApp(MaterialApp(
+      title: "Aplikasi Flutter",
+      home: Scaffold(
+        appBar: AppBar(title: Text('Aplikasi Flutter'),),
+        body: Material(
+          color: Colors.white,
+          child: Center(
+            child: Text("Hello World !", textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.deepPurple, fontSize: 40.0),
+            ),
+          ),
+        ),
+      )
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,12 +23,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // T ry running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.pink,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Splashscreen(),
+      home: MyHomePage(title: 'Halo Keren'),
     );
   }
 }
@@ -30,8 +49,7 @@ class MyHomePage extends StatefulWidget {
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
+  // how it looks
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
@@ -57,21 +75,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  /*void navigateLogin() async{
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    int isLogin = pref.getInt("is_login");
-    if(isLogin == 1){
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => tugaspertemuan8(title: "Halo Push",)),
-      );
-    }
-  }
-  @override
-  void initState() {
-    navigateLogin();
-  }*/
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -90,6 +93,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -99,19 +116,6 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            /*RaisedButton(
-              child: Text(
-                'LOGIN'
-              ),
-              onPressed: () async {
-                SharedPreferences pref = await SharedPreferences.getInstance();
-                await pref.setInt("is_login", 1);
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => pertemuan1(title: "Halo Push",)),
-                );
-              },
-            )*/
           ],
         ),
       ),
