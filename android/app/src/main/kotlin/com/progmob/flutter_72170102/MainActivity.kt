a729b64e36ab647f86ff66856d4c161ca7e3f92a
avelinamale28 <62369253+avelinamale28@users.noreply.github.com>
2020-11-29 19:56:51 +0700

    tugas9_Flutter navigation & preferences

---
+++ android/app/src/main/kotlin/com/progmob/flutter_72170102/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.progmob.flutter_72170102
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
-      home: MyHomePage(title: 'Flutter Progmob 72170109 TugasPertemuan8'),
+      home: MyHomePage(title: 'Flutter 721701'),
       debugShowCheckedModeBanner: false,
     );
   }
