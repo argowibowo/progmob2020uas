97a5c49839d5ed3aa92ae1033fb4bb4261ea419a
Louis Halawa <louishalawa@gmail.com>
2020-12-01 01:01:19 +0700

    Tugas 10 / 72180233

---
+++ lib/datamatakuliah.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Matakuliah extends StatefulWidget {
+  Matakuliah({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MatakuliahState createState() => _MatakuliahState();
+}
+
+class _MatakuliahState extends State<Matakuliah> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {}
+          )
+        ],
+      ),
+
+      body: Container(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Algoritma dan Struktur Data"),
+                subtitle: Text("SI1423"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (_) => <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>(
+                      child: const Text('Update'), value: 'Update',
+                    ),
+                    new PopupMenuItem<String>(
+                      child: const Text('Delete'), value: 'Delete',
+                    )
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Analisis Proses Bisnis"),
+                subtitle: Text("SI1453"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (_) => <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>(
+                      child: const Text('Update'), value: 'Update',
+                    ),
+                    new PopupMenuItem<String>(
+                      child: const Text('Delete'), value: 'Delete',
+                    )
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Sistem Basis Data"),
+                subtitle: Text("SI1443"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (_) => <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>(
+                      child: const Text('Update'), value: 'Update',
+                    ),
+                    new PopupMenuItem<String>(
+                      child: const Text('Delete'), value: 'Delete',
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:progmob2020/datadosen.dart';
+import 'package:progmob2020/datamahasiswa.dart';
+import 'package:progmob2020/datamatakuliah.dart';
 import 'package:progmob2020/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
-
-
-
   final String title;
 
   @override
@@ -15,47 +16,90 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
+  final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            RaisedButton(
-                color: Colors.blueAccent,
-                onPressed: () async{
-                  SharedPreferences pref = await SharedPreferences.getInstance();
-                  await pref.setInt("is_login", 0);
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => Login(title: "Login"))
-                  );
-                },
+            UserAccountsDrawerHeader(
+              accountName: Text("Louis Haga Alnoveus Halawa"),
+              accountEmail: Text("louishalawa@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.red,
                 child: Text(
-                  "Logout",
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                )
+                  "LH",
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Logout"),
+              trailing: Icon(Icons.exit_to_app),
+              onTap: () async {
+                SharedPreferences pref = await SharedPreferences.getInstance();
+                await pref.setInt("is_login", 0);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login(title: "Halaman Login",)),
+                );
+              },
             ),
           ],
         ),
       ),
+
+      body: Container(
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text("Dosen"),
+                subtitle: Text("Menu Dosen"),
+                trailing: Icon(Icons.people),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Dosen(title: "Data Dosen"))
+                  );
+                },
+              ),
+              ListTile(
+                title: Text("Mahasiswa"),
+                subtitle: Text("Menu Mahasiswa"),
+                trailing: Icon(Icons.people_outline),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Mahasiswa(title: "Data Mahasiswa"))
+                  );
+                },
+              ),
+              ListTile(
+                title: Text("Matakuliah"),
+                subtitle: Text("Menu Matakuliah"),
+                trailing: Icon(Icons.book_sharp),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Matakuliah(title: "Data Matakuliah"))
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
