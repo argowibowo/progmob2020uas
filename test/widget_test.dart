f8e9bcfed7bceb96c5ea963d64d3307b34a115ea
kerenkezia12 <keren.kezia@si.ukdw.ac.id>
2020-12-02 03:57:52 +0700

    tugas 10

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_keren/main.dart';
+import 'package:flutter_kerenkezia/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
