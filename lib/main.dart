a8cffb1f9683695ad6918cba067c3118b57cd0c4
kerenkezia12 <keren.kezia@si.ukdw.ac.id>
2020-12-02 03:48:07 +0700

    tugas 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kerenkezia/Splashscreen.dart';
+import 'package:flutter_kerenkezia/splash.dart';
 
 void main() {
   runApp(MyApp());
