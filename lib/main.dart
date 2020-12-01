8041001cefc7e7bc78e14cf53ba5fe76a16837ed
Aji Wibowo <aji.wibowo@si.ukdw.ac.id>
2020-12-01 18:44:24 +0700

    Commit Tugas Pertemuan 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_progmob2020/pertemuan8.dart';
+import 'package:flutter_progmob2020/pertemuan2.dart';
+import 'package:flutter_progmob2020/tugas8.dart';
+import 'package:flutter_progmob2020/login.dart';
 
-void main() => runApp(MyApp());
-
+void main() {runApp(MyApp()  );
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tampilan ',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
@@ -75,20 +72,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -98,6 +81,18 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            // RaisedButton(
+            //   child: Text(
+            //     'Pindah Halaman'
+            //   ),
+            //     onPressed: () {
+            //       Navigator.pushReplacement( // membuat tidak ada tombol back pada interface
+            //         context,
+            //         MaterialPageRoute(builder: (context) => Pertemuan8(title: 'Halaman Kedua',)),
+            //       );
+            //     }
+            // )
+
           ],
         ),
       ),
---
+++ lib/tugas8.dart
@@ -0,0 +1,115 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:flutter_progmob2020/login.dart';
+import 'package:flutter_progmob2020/pertemuan2.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main.dart';
+
+class tugas8 extends StatefulWidget {
+  tugas8({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _tugas8State createState() => _tugas8State();
+}
+
+class _tugas8State extends State<tugas8> {
+  GlobalKey<FormState> key = GlobalKey<FormState>();
+
+  Void Validate(){
+    if (key.currentState.validate()){
+      print("Validate");
+    }
+    else {
+      print("failed");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tugas Pertemuan ke-8"),
+      ),
+      body: Padding(
+        padding:EdgeInsets.all(15.0),
+        child : Center(
+            child : Form(
+              key: key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Nama Tidak Boleh Kosong";
+                      }
+                      else{
+                        return null;
+                      }
+                    },
+                    decoration: new InputDecoration(
+                      icon:Icon (
+                        Icons.account_circle_rounded,
+                        color: Colors.blue,
+                      ),
+                      labelText: 'Nama Lengkap',
+                      hintText: 'Contoh: Aji Wibowo',
+                      border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(3),
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    onPressed: Validate,
+                    child: Text(
+                      "Submit",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    child: Text(
+                      "Logout",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                    onPressed: () async {
+                      SharedPreferences prefs = await SharedPreferences.getInstance();
+                      await prefs.setInt("isLogin", 0);
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => login(title: 'Login Page')),
+                      );
+                    },
+                  )
+                ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+
+  void checkLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int isLogin = prefs.getInt('isLogin');
+    if(isLogin == 0) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage(title: 'Page Login ')),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    checkLogin();
+  }
+}
