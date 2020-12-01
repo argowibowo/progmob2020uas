54b80868a857978f57ec2e896d56d2c5b39b6947
kerenkezia12 <keren.kezia@si.ukdw.ac.id>
2020-12-02 03:24:10 +0700

    tugas 10

---
+++ lib/Dosen/dashboarddosen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class dashboarddosen extends StatefulWidget {
+  dashboarddosen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _dashboarddosenState createState() => _dashboarddosenState();
+}
+
+class _dashboarddosenState extends State<dashboarddosen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: (){},
+            )
+          ]
+      ),
+
+      body: Container(
+        child: Card(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children:<Widget>[
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("Drs. Jong Jek Siang, M.Sc."),
+                  subtitle: Text("086890 - jongjeksiang@staff.ukdw.ac.id"),
+                  onLongPress: (){
+                    showDialog(
+                        context: context,
+                        builder: (_) => new AlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              FlatButton(
+                                child: Text("Update"),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              FlatButton(
+                                child: Text("Delete"),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                },
+                              )
+                            ],
+                          ),
+                        )
+                    );
+                  },
+                ),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
