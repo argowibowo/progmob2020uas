54af7e2a8dd3b1470e2d04bd921c9eb9d0ea871c
jonathan_mata <jonathan.leonardo@si.ukdw.ac.id>
2020-12-03 12:57:31 +0700

    tugas 10

---
+++ lib/mahasiswa/dashboardmhs.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DashboardMhs extends StatefulWidget {
+  DashboardMhs({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _DashboardMhsState createState() => _DashboardMhsState();
+}
+
+class _DashboardMhsState extends State<DashboardMhs> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {}
+          )
+        ],
+      ),
+
+      body: Container(
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Jonathan Mata"),
+                subtitle: Text("72170099 - jonathan.mata@si.ukdw.ac.id"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (_) => <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>(
+                      child: const Text('Update'), value: 'Update',
+                    ),
+                    new PopupMenuItem<String>(
+                      child: const Text('Delete'), value: 'Delete',
+                    )
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Lawliet"),
+                subtitle: Text("72170099 - Lawliet@si.ukdw.ac.id"),
+                trailing: PopupMenuButton(
+                  itemBuilder: (_) => <PopupMenuItem<String>>[
+                    new PopupMenuItem<String>(
+                      child: const Text('Update'), value: 'Update',
+                    ),
+                    new PopupMenuItem<String>(
+                      child: const Text('Delete'), value: 'Delete',
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: Icon(Icons.add),
+      // ),
+    );
+  }
+}
