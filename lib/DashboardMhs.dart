01daea4df998d6df24833e3aca6d24cb38b724ab
ribkashn <ribkatiara2000@gmail.com>
2020-12-11 22:48:11 +0700

    Api dan upload foto

--- lib/DashboardMhs.dart
+++ lib/DashboardMhs.dart
@@ -1,13 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_app/apiservices.dart';
+import 'package:flutter_app/mahasiswa/addmhs.dart';
+import 'package:flutter_app/mahasiswa/updatemhs.dart';
+import 'package:flutter_app/model.dart';
+
+//import add
+//import update
 
 class DashboardMhs extends StatefulWidget {
   DashboardMhs({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _DashboardMhsState createState()=> _DashboardMhsState();
 }
 class _DashboardMhsState extends State<DashboardMhs> {
   final _formKey = GlobalKey<FormState>();
+
+  List<Mahasiswa> lMhs= new List();
+
+  //berfungsi ketika menginsert data baru dan akan merefresh data yang sudah di ubah
+  FutureOr onGoBack(dynamic value){
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,39 +34,83 @@ class _DashboardMhsState extends State<DashboardMhs> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: (){},
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=> AddMhs(title :"Input Data Mahasiswa")),
+              ).then(onGoBack);
+            },
           ),
         ],
       ),
-      body: Container(
-        child: GestureDetector(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Ribka Nita Tiara"),
-                  subtitle: Text("72180246 - ribka.tiara@si.ukdw.ac.id"),
-                  trailing: PopupMenuButton(
-                    itemBuilder: (_) => <PopupMenuItem<String>>[
-                      new PopupMenuItem<String>(
-                        child: const Text('Update'), value: 'Update',
+      body: FutureBuilder(
+        future: ApiServices().getMahasiswa(),
+        builder: (BuildContext context, AsyncSnapshot<List<Mahasiswa>> snapshot){
+          if (snapshot.hasError){
+            return Center(
+              child: Text(
+                "Something Wrong With message: ${snapshot.error.toString()}"),
+              );
+          }else if (snapshot.connectionState == ConnectionState.done){
+            lMhs = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, position) {
+                return Card( /////////////copy nanti bagian card yang sudah dibuat minggu lalu
+                  //elevation: 8.0,
+                  margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 1.0),
+                  child: Container(
+                    child: ListTile(
+                      title: Text(lMhs[position].nama + " - " + lMhs[position].nim),
+                      subtitle: Text(lMhs[position].email),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(lMhs[position].foto),
                       ),
-                      new PopupMenuItem<String>(
-                        child: const Text('Delete'), value: 'Delete',
-                      ),
-                      new PopupMenuItem<String>(
-                        child: const Text('Edit'), value: 'Edit',
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+                      onLongPress: (){
+                        showDialog(context: context,
+                        builder: (_) => new AlertDialog(
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              FlatButton(
+                                child: Text("Update"),
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context)=> UpdateMhs(title: "Input Data Mahasiswa", mhs: lMhs[position], nimcari: lMhs[position].nim)),
+                                  ).then(onGoBack);
+                                },
+                              ),
+                              Divider(
+                                color: Colors.black,
+                                height: 20,
+                              ),
+                              FlatButton(
+                                child: Text("Delete"),
+                                onPressed: () async{
+                                  ApiServices().deleteMhs(lMhs[position].nim);
+                                  Navigator.pop(context);
+                                  setState(() {});
+                                },
+                              )
+                            ],
+                          ),
+                        )
+                        );
+                      },
+                    ),
+                  )
+                );
+              },
+              itemCount: lMhs.length,
+            );
+          } else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      )
     );
   }
 }
---
+++ lib/model.dart
@@ -0,0 +1,63 @@
+
+import 'dart:convert';
+
+import 'package:charts_flutter/flutter.dart';
+///chart
+//class ClicksPerYear {
+ // final String year;
+  //final int clicks;
+  //final charts.Color color;
+
+  //ClicksPerYear(this.year, this.clicks, Color color)
+     //:this.color = charts.Color(
+ //     r: color.red, g: color.green, b: color.blue, a: color.alpha);
+//  )
+
+//}
+
+//----------dashboard
+class DashboardSI{
+  String mahasiswa;
+  // String dosen;
+  // String matakuliah;
+  // String jadwal;
+
+  DashboardSI({this.mahasiswa});
+
+  factory DashboardSI.fromJson(Map<String, dynamic> json){
+    return DashboardSI(
+      mahasiswa: json["mahasiswa"] // tambahkan dosen matkul dan jadwal
+    );
+  }
+}
+//-----------daftar mahasiswa
+class Mahasiswa{
+  String id;
+  String nama;
+  String nim;
+  String alamat;
+  String email;
+  String foto;
+  String nim_progmob;
+
+  Mahasiswa({this.id, this.nama, this.nim, this.alamat, this.email, this.foto, this.nim_progmob});
+  factory Mahasiswa.fromJson(Map<String, dynamic> map){
+    return Mahasiswa(id: map["id"], nama: map["nama"], nim: map["nim"], alamat: map["alamat"],
+        email: map["email"], foto: map["foto"], nim_progmob: map["nim_progmob"]);
+  }
+  Map<String, dynamic> toJson(){
+    return{"id": id, "nama": nama, "nim": nim, "alamat": alamat, "email": email, "foto": foto, "nim_progmob": nim_progmob};
+}
+}
+List<Mahasiswa> mahasiswaFromJson(String jsonData){
+  final data = json.decode(jsonData);
+  return List<Mahasiswa>.from(data.map((item) => Mahasiswa.fromJson(item)));
+}
+String mahasiswaToJson(Mahasiswa data){
+  final jsonData = data.toJson();
+  return json.encode(jsonData);
+}
+//-------- dosen
+//-------matkul
+//---------jadwal
+//-----todoo
