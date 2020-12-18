60d78e4bd97415a73a3c77b6f5211aa4b739d4a2
anisyaclara <anisya.clara@si.ukdw.ac.id>
2020-12-18 22:15:03 +0700

    CRUD Mahasiswa

--- lib/mahasiswa/dashboardmahasiswa.dart
+++ lib/mahasiswa/dashboardmahasiswa.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:progmob_flutter/model.dart';
+import 'package:progmob_flutter/apiservices.dart';
+import 'package:progmob_flutter/mahasiswa/addmhs.dart';
+import 'package:progmob_flutter/mahasiswa/updatemhs.dart';
 
 class DashboardMahasiswa extends StatefulWidget {
   DashboardMahasiswa({Key key, this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -11,46 +17,98 @@ class DashboardMahasiswa extends StatefulWidget {
 class _DashboardMahasiswaState extends State<DashboardMahasiswa> {
   final _formKey = GlobalKey<FormState>();
 
+  List<Mahasiswa> lMhs = new List();
+
+  FutureOr onGoBack(dynamic value){
+    setState(() {});
+  }
+
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.add),
-            onPressed: (){},
-          ),
+            icon: Icon(Icons.person_add),
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddMhs(title: "Tambah Data Mahasiswa")),
+              ).then(onGoBack);
+            },
+          )
         ],
       ),
-      body: Container(
-        child: GestureDetector(
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Anisya Clara Br Sitinjak"),
-                  subtitle: Text("72180260 - anisya.clara@si.ukdw.ac.id"),
-                  trailing: PopupMenuButton(
-                    itemBuilder: (_) => <PopupMenuItem<String>>[
-                      new PopupMenuItem<String>(
-                        child: const Text('Update'), value: 'Update',
+      body: FutureBuilder(
+        future: ApiServices().getMahasiswa(),
+        builder: (BuildContext context, AsyncSnapshot<List<Mahasiswa>> snapshot){
+          if(snapshot.hasError){
+            return Center(
+              child: Text(
+                  "Something Wrong with message: ${snapshot.error.toString()}"
+              ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.done){
+            lMhs = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, position){
+                return Card(
+                  margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 1.0),
+                  child: Container(
+                    child: ListTile(
+                      title: Text(lMhs[position].nama + " - " + lMhs[position].nim),
+                      subtitle: Text(lMhs[position].email),
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(lMhs[position].foto),
                       ),
-                      new PopupMenuItem<String>(
-                        child: const Text('Delete'), value: 'Delete',
-                      ),
-                      new PopupMenuItem<String>(
-                        child: const Text('Edit'), value: 'Edit',
-                      ),
-                    ],
-                  )
-                ),
-              ],
-            ),
-          ),
-        )
+                      onLongPress: (){
+                        showDialog(
+                            context: context,
+                            builder: (_) => new AlertDialog(
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  FlatButton(
+                                    child: Text("Update"),
+                                    onPressed: (){
+                                      Navigator.pop(context);
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context) => UpdateMhs(title: "Update Mahasiswa",
+                                            mhs:lMhs[position], nimcari: lMhs[position].nim)),
+                                      ).then(onGoBack);
+                                    },
+                                  ),
+                                  Divider(
+                                    color: Colors.black,
+                                    height: 20,
+                                  ),
+                                  FlatButton(
+                                      child: Text("Delete"),
+                                      onPressed: () async{
+                                        ApiServices().deleteMhs(lMhs[position].nim);
+                                        Navigator.pop(context);
+                                        setState(() {});
+                                      })
+                                ],
+                              ),
+                            )
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+              itemCount: lMhs.length,
+            );
+          } else{
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
