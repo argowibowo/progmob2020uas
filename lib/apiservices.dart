8674059a8c4434bc3191dc09c98f53455612cf3b
LAPTOP-1LR8SJID\lenovo <fransisca.angelina@students.ukdw.ac.id>
2020-12-18 20:08:05 +0700

    TAS

---
+++ lib/apiservices.dart
@@ -0,0 +1,335 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter_72180185/model.dart';
+import 'package:http/http.dart' show Client;
+import 'package:http/http.dart' as http;
+
+
+class ApiServices {
+  final String baseUrl = "https://argouchiha.000webhostapp.com";
+
+  Client client = Client();
+
+
+  //dashboard
+  Future<DashboardSI> getDashboard() async {
+    final response = await client.get(
+        "$baseUrl/api/progmob/dashboard/72180185");
+    if (response.statusCode == 200) {
+      return DashboardSI.fromJson(json.decode(response.body));
+    } else {
+      return null;
+    }
+  }
+
+  //mahasiswa
+  Future<List<Mahasiswa>> getMahasiswa() async {
+    final response = await client.get("$baseUrl/api/progmob/mhs/72180185");
+    if (response.statusCode == 200) {
+      return mahasiswaFromJson(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> createMhs(Mahasiswa data) async {
+    final response = await client.post(
+      "$baseUrl/api/progmob/mhs/createwithfoto",
+      headers: {"content-type": "application/json; multipart/form-data"},
+      body: mahasiswaToJson(data),
+    );
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> createMhsWithFoto(Mahasiswa data, File file,
+      String fileName) async {
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/api/progmob/mhs/createwithfoto")
+    );
+
+    Map<String, String> headers = {
+      "Content-type": "multipart/form-data"
+    };
+
+    request.headers.addAll(headers);
+    request.files.add(
+        http.MultipartFile(
+            "foto",
+            file.readAsBytes().asStream(),
+            file.lengthSync(),
+            filename: fileName
+        )
+    );
+    request.fields.addAll({
+      "nama": data.nama,
+      "nim": data.nim,
+      "alamat": data.alamat,
+      "email": data.email,
+      "nim_progmob": data.nim_progmob
+    });
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updateMhsWithFoto(Mahasiswa data, File file,
+      String nimcari) async {
+    String isfotoupdate = "0";
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/api/progmob/mhs/updatewithfoto")
+    );
+
+    Map<String, String> headers = {
+      "Content-type": "multipart/form-data"
+    };
+
+    request.headers.addAll(headers);
+
+    if (file != null) {
+      request.files.add(
+          http.MultipartFile(
+              "foto",
+              file.readAsBytes().asStream(),
+              file.lengthSync(),
+              filename: file.path
+          )
+      );
+      isfotoupdate = "1";
+    }
+
+    request.fields.addAll({
+      "nama": data.nama,
+      "nim": data.nim,
+      "alamat": data.alamat,
+      "email": data.email,
+      "nim_progmob": data.nim_progmob,
+      "nim_cari": nimcari,
+      "is_foto_update": isfotoupdate
+    });
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> deleteMhs(String nim) async {
+    final response = await client.post(
+        "$baseUrl/api/progmob/mhs/delete",
+        headers: {"content-type": "application/json"},
+        body: jsonEncode(<String, String>{
+          "nim": nim,
+          "nim_progmob": "72180185"
+        })
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //dosen
+  Future<List<Dosen>> getDosen() async {
+    final response = await client.get("$baseUrl/api/progmob/dosen/72180185");
+    if (response.statusCode == 200) {
+      return dosenFromJson(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> createDosen(Mahasiswa data) async {
+    final response = await client.post(
+        "$baseUrl/api/progmob/dosen/create",
+        headers: {"content-type": "application/json"},
+        body: mahasiswaToJson(data)
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> createDosenWithFoto(Dosen data, File file,
+      String filename) async {
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/api/progmob/dosen/createwithfoto")
+    );
+
+    Map<String, String> headers = {
+      "Content-type": "multipart/form-data"
+    };
+
+    request.headers.addAll(headers);
+
+    request.files.add(
+        http.MultipartFile(
+            "foto",
+            file.readAsBytes().asStream(),
+            file.lengthSync(),
+            filename: filename
+        )
+    );
+
+    request.fields.addAll({
+      "nama": data.nama,
+      "nidn": data.nidn,
+      "alamat": data.alamat,
+      "email": data.email,
+      "gelar": data.gelar,
+      "nim_progmob": data.nim_progmob
+    });
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updateDosenWithFoto(Dosen data, File file,
+      String nidn_cari) async {
+    String isFotoUpdate = "0";
+
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/api/progmob/dosen/updatewithfoto")
+    );
+
+    Map<String, String> headers = {
+      "Content-type": "multipart/form-data"
+    };
+
+    request.headers.addAll(headers);
+
+    if (file != null) {
+      request.files.add(
+          http.MultipartFile(
+              "foto",
+              file.readAsBytes().asStream(),
+              file.lengthSync(),
+              filename: file.path
+          )
+      );
+      isFotoUpdate = "1";
+    }
+
+    request.fields.addAll({
+      "nama": data.nama,
+      "nidn": data.nidn,
+      "alamat": data.alamat,
+      "email": data.email,
+      "gelar": data.gelar,
+      "nim_progmob": data.nim_progmob,
+      "nidn_cari": nidn_cari,
+      "is_foto_update": isFotoUpdate
+    });
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> deleteDosen(String nidn) async {
+    final response = await client.post(
+        "$baseUrl/api/progmob/dosen/delete",
+        headers: {"content-type": "application/json"},
+        body: json.encode(<String, String>{
+          "nidn": nidn,
+          "nim_progmob": "72180185"
+        })
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //matakuliah
+  Future<List<Matakuliah>> getMatkul() async {
+    final response = await client.get("$baseUrl/api/progmob/matkul/72180185");
+    if (response.statusCode == 200) {
+      return matkulFromJson(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  Future<bool> createMatkul(Matakuliah data) async {
+    final response = await client.post(
+        "$baseUrl/api/progmob/matkul/create",
+        headers: {"content-type": "application/json"},
+        body: matkulToJson(data)
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> updateMatkul(Matakuliah data, String kode_cari) async {
+    var request = http.MultipartRequest(
+        'POST',
+        Uri.parse("$baseUrl/api/progmob/matkul/update")
+    );
+
+    request.fields.addAll({
+      "nama": data.nama,
+      "nim_progmob": data.nim_progmob,
+      "kode": data.kode,
+      "hari": data.hari.toString(),
+      "sesi": data.sesi.toString(),
+      "sks": data.sks.toString(),
+      "kode_cari": kode_cari
+    });
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> deleteMatkul(String kode) async {
+    final response = await client.post(
+        "$baseUrl/api/progmob/matkul/delete",
+        headers: {"content-type": "application/json"},
+        body: json.encode(<String, String>{
+          "kode": kode,
+          "nim_progmob": "72180185"
+        })
+    );
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
--- lib/mahasiswa/dashboardmhs.dart
+++ lib/mahasiswa/dashboardmhs.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_72180185/apiservices.dart';
+import 'package:flutter_72180185/mahasiswa/addmhs.dart';
+import 'package:flutter_72180185/mahasiswa/updatemhs.dart';
+import 'package:flutter_72180185/model.dart';
+
 
 class DashboardMhs extends StatefulWidget {
   DashboardMhs({Key key, this.title}) : super(key: key);
@@ -8,65 +13,101 @@ class DashboardMhs extends StatefulWidget {
   @override
   _DashboardMhsState createState() => _DashboardMhsState();
 }
+
 class _DashboardMhsState extends State<DashboardMhs> {
   final _formKey = GlobalKey<FormState>();
 
-  @override
-  void initState() {
-    super.initState();
+  List<Mahasiswa> lMhs = new List();
+
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: (){},
-            ),
-          ],
+            title: Text(widget.title),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddMhs(title: "Input Data Mahasiswa")),
+                  ).then(onGoBack);
+                },
+              )
+            ]
         ),
-        body: Container(
-            child: GestureDetector(
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.person),
-                        title: Text("Fransisca Fethy Angelina"),
-                        subtitle: Text("72180185 - fransisca.fethy@si.ukdw.ac.id"),
-                        onLongPress: (){
-                          showDialog(
-                              context: context,
-                              builder: (_) => new AlertDialog(
-                                content: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    FlatButton(
-                                      child: Text("Update"),
-                                      onPressed: (){
-                                        Navigator.pop(context);
-                                      },
+        body: FutureBuilder(
+            future: ApiServices().getMahasiswa(),
+            builder: (BuildContext context, AsyncSnapshot<List<Mahasiswa>> snapshot){
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                      "Something wrong with message: ${snapshot.error.toString()}"),
+                );
+              }else if (snapshot.connectionState == ConnectionState.done) {
+                lMhs = snapshot.data;
+                return ListView.builder(
+                  itemBuilder: (context, position) {
+                    return Card(
+                        margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 1.0),
+                        child: Container(
+                          child:ListTile(
+                            title: Text(lMhs[position].nama + " - " + lMhs[position].nim),
+                            subtitle: Text(lMhs[position].email),
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(lMhs[position].foto),
+                            ),
+                            onLongPress: (){
+                              showDialog(
+                                  context: context,
+                                  builder: (_) => new AlertDialog(
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        FlatButton(
+                                          child: Text("Update"),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(builder: (context) => UpdateMhs(
+                                                  title: "Input Data Mahasiswa", mhs: lMhs[position], nimcari: lMhs[position].nim)),
+                                            ).then(onGoBack);
+                                          },
+                                        ),
+                                        Divider(
+                                          color: Colors.black,
+                                          height: 20,
+                                        ),
+                                        FlatButton(
+                                          child: Text("Delete"),
+                                          onPressed: () async {
+                                            ApiServices().deleteMhs(lMhs[position].nim);
+                                            Navigator.pop(context);
+                                            setState(() {});
+                                          },
+                                        )
+                                      ],
                                     ),
-                                    FlatButton(
-                                      child: Text("Delete"),
-                                      onPressed: (){
-                                        Navigator.pop(context);
-                                      },
-                                    )
-                                  ],
-                                ),
-                              )
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                )
-            )
+                                  )
+                              );
+                            },
+                          ),
+                        )
+                    );
+                  },
+                  itemCount: lMhs.length,
+                );
+              }else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }
         )
     );
   }
