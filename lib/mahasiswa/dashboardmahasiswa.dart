e2aba76c7b9082dc64e15fb7c4b7ccb8cb24fa29
LAPTOP-HJQKF6RD\asus <diorypanjaitan1122@gmail.com>
2020-12-18 13:44:37 +0700

    Tugas 11: Flutter API & Image Upload

--- lib/mahasiswa/dashboardmahasiswa.dart
+++ lib/mahasiswa/dashboardmahasiswa.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_progmob/apiservices.dart';
+import 'package:flutter_progmob/mahasiswa/addmhs.dart';
+import 'package:flutter_progmob/mahasiswa/updatemhs.dart';
 import 'package:flutter_progmob/main.dart';
+import 'package:flutter_progmob/model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DashboardMahasiswa extends StatefulWidget {
@@ -13,44 +18,97 @@ class DashboardMahasiswa extends StatefulWidget {
 class _DashboardMahasiswaState extends State<DashboardMahasiswa> {
   final _formKey = GlobalKey<FormState>();
 
+  List<Mahasiswa> lMhs = new List();
+
+  FutureOr onGoBack(dynamic value) {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: (){},
-          )
-        ],
-      ),
-      body: Container(
-          child: GestureDetector(
-            child: Card(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget> [
-                    ListTile(
-                      leading: Icon(Icons.person),
-                      title: Text("Nanon Korapat"),
-                      subtitle: Text("72180210 - nanon@gmail.com"),
-                      trailing: PopupMenuButton(
-                        itemBuilder: (_) => <PopupMenuItem<String>>[
-                          new PopupMenuItem<String>(
-                              child: const Text('Update'), value: 'Doge'),
-                          new PopupMenuItem<String>(
-                            child: const Text('Delete'), value: 'Lion',
+        appBar: AppBar(
+          title: Text(widget.title),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                  onPressed: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AddMhs(title: "Input Data Mahasiswa")),
+                    ).then(onGoBack);
+                  },
+              )
+            ]
+        ),
+        body: FutureBuilder(
+            future: ApiServices().getMahasiswa(),
+            builder: (BuildContext context, AsyncSnapshot<List<Mahasiswa>> snapshot){
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                      "Something wrong with message: ${snapshot.error.toString()}"),
+                );
+              }else if (snapshot.connectionState == ConnectionState.done) {
+                lMhs = snapshot.data;
+                return ListView.builder(
+                    itemBuilder: (context, position) {
+                      return Card(
+                          margin: new EdgeInsets.symmetric(horizontal: 5.0, vertical: 1.0),
+                          child: Container(
+                              child:ListTile(
+                                  title: Text(lMhs[position].nama + " - " + lMhs[position].nim),
+                                  subtitle: Text(lMhs[position].email),
+                                  leading: CircleAvatar(
+                                    backgroundImage: NetworkImage(lMhs[position].foto),
+                                  ),
+                                  onLongPress: (){
+                                    showDialog(
+                                        context: context,
+                                        builder: (_) => new AlertDialog(
+                                          content: Column(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              FlatButton(
+                                                child: Text("Update"),
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                  Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(builder: (context) => UpdateMhs(title: "Input Data Mahasiswa", mhs: lMhs[position], nimcari: lMhs[position].nim)),
+                                                  ).then(onGoBack);
+                                                  },
+                                              ),
+                                              Divider(
+                                                color: Colors.black,
+                                                height: 20,
+                                              ),
+                                              FlatButton(
+                                                child: Text("Delete"),
+                                                onPressed: () async {
+                                                  ApiServices().deleteMhs(lMhs[position].nim);
+                                                  Navigator.pop(context);
+                                                  setState(() {});
+                                                  },
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                    );
+                                  },
+                                  ),
                           )
-                        ],
-                      ),
-                    )
-                  ],
-                )
-            ),
-          )
-      ),
+                      );
+                    },
+                  itemCount: lMhs.length,
+                );
+              }else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }
+            )
     );
-
   }
 }
